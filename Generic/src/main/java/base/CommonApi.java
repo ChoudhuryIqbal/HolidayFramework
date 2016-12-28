d9d6c20c17462e8a5d4c11a39208141ec96f309b
iqbal <iqbalchoudhury2@gmail.com>
2016-12-27 20:36:45 -0500

    Cloud config

--- Generic/src/main/java/base/CommonApi.java
+++ Generic/src/main/java/base/CommonApi.java
@@ -4,12 +4,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -20,13 +27,71 @@ import java.util.concurrent.TimeUnit;
 public class CommonApi {
     public WebDriver driver=null;
 
-    @Parameters({"url"})
-    @BeforeMethod
+   // @Parameters({"url"})
+    @Parameters({"usecloud","userName","accessKey","os","browserName","browserVersion","url"})
+   /* @BeforeMethod
     public void setUp(String url){
         driver=new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(url);
         driver.manage().window().maximize();
+    }*/
+
+
+    @BeforeMethod
+    public void setUp(@Optional("false") boolean useCloud,@Optional("justmeiqbal ") String userName,@Optional("0b3a4b38-5376-4760-b444-21011462a1e7 ") String accessKey,
+                      @Optional ("Windows 8") String os,@Optional ("firefox") String browserName,@Optional("40") String
+                      browserVersion,@Optional("http://www.cnn.com") String url)throws IOException{
+        if (useCloud==true){
+            //run in cloud
+            getCloudDriver(userName,accessKey,os,browserName,browserVersion);
+        }
+        else
+        {
+            //run local
+            getLocalDriver(browserName);
+        }
+
+
+        driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+        driver.get(url);
+        driver.manage().window().maximize();
+    }
+
+
+
+    //beging getLocal Driver
+    public WebDriver getLocalDriver(String browserName){
+        if(browserName.equalsIgnoreCase("chrome")){
+            System.setProperty("webdriver.chrome.driver","C:\\Users\\a\\udemyAutomation\\HolidayAutomation\\Generic\\browser-driver\\chromedriver.exe");
+            driver=new ChromeDriver();
+
+        }
+        else if(browserName.equalsIgnoreCase("firefox")){
+            driver=new FirefoxDriver();
+
+        }
+        else if(browserName.equalsIgnoreCase("ie")){
+            System.setProperty("webdriver.ie.driver","C:\\Users\\a\\udemyAutomation\\HolidayAutomation\\Generic\\browser-driver\\IEDriverServer.exe");
+            driver=new InternetExplorerDriver();
+
+        }
+
+
+        return driver;
+    }
+
+    public WebDriver getCloudDriver(String userName,String accessKey,String os,String browserName,String browserVersion
+                                    )throws IOException{
+        DesiredCapabilities cap=new DesiredCapabilities();
+        cap.setCapability("platform",os);
+        cap.setBrowserName(browserName);
+        cap.setCapability("version",browserVersion);
+        driver=new RemoteWebDriver(new URL("http://"+userName+":"+accessKey+"@ondemand.saucelabs.com:80/wd/hub"),cap);
+
+
+
+        return driver;
     }
     @AfterMethod
     public void cleanUp(){
